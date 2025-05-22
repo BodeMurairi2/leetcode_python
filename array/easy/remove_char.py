c329e472c0c16f2534691d41d11f1a107513933e
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-22 13:23:18 +0200

    remove char challenge

---
+++ array/easy/remove_char.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def filter_list(l):
+    new_list = []
+    for char in l:
+        if isinstance(char, int):
+            new_list.append(char)
+    return new_list
+
+print(filter_list(l= [1, 5, 6, "bode", "dirac", "123", "programming", 10]))
